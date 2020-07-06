224140b6c02c2c3365b2a1f416a2be18882da68a
script90 <57960274+script90@users.noreply.github.com>
2020-07-06 11:47:28 -0300

    Update proxy.py

--- scripts/proxy.py
+++ scripts/proxy.py
@@ -14,10 +14,8 @@ PASS = ''
 BUFLEN = 8196 * 8
 TIMEOUT = 60
 MSG = 'SSHPLUS'
-COR = '<font color="null">'
-FTAG = '</font>'
 DEFAULT_HOST = '0.0.0.0:22'
-RESPONSE = "HTTP/1.1 200 " + str(COR) + str(MSG) + str(FTAG) + "\r\n\r\n"
+RESPONSE = "HTTP/1.1 200 " + str(MSG) + "\r\n\r\n"
  
 class Server(threading.Thread):
     def __init__(self, host, port):
